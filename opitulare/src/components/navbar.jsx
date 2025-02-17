110f4c7a1c79fe78f2ffe8b57144271396317565
Yuri <142381932+Yurei04@users.noreply.github.com>
2025-02-17 13:40:35 +0800

    Navigation update
    - Updating Navbar both UI and UX
    - Icon add later on

--- opitulare/src/components/navbar.jsx
+++ opitulare/src/components/navbar.jsx
@@ -4,6 +4,8 @@ import {
     Box, 
     Button,
     Container, 
+    IconButton, 
+    ToggleButton, 
     Toolbar 
 
 } from "@mui/material";
@@ -37,8 +39,50 @@ const Navbar = () => {
             }}
         >
             <Container maxWidth="lg">
-                <StyledToolbar variant="dense">
+                <StyledToolbar variant="dense" disableGutters>
+                    <Box sx={{ display: "flex", flexGrow: 1, alignItems: "center", px: 0  }}>
+                        <Logo/>
+                        <Box sx={{ display: { xs: "none", md: "flex"}}}>
+                            <Button variant="text" color="info" size="small" >
+                                Home
+                            </Button>
+                            <Button variant="text" color="info" size="small" >
+                                About
+                            </Button>
+                            <Button variant="text" color="info" size="small" >
+                                Contanct
+                            </Button>
+                            <Button variant="text" color="info" size="small" >
+                                Help
+                            </Button>
+                        </Box>
+                    </Box>
 
+                    <Box 
+                        sx={{
+                            display: { sx: "none", md: "flex"},
+                            gap: 1,
+                            alignItems: "center"
+                        }}
+                    >
+
+                        <Button color="primary" variant="text" size="small">
+                            Login
+                        </Button>
+                        <Button color="primary" variant="text" size="small">
+                            Signup
+                        </Button>
+                    </Box>
+                    <ColorModeIconDropDown /> /* Add Later */
+                    
+                    <Box sx={{ display: {sx: "flex", md: "none"}, gap: 1}}>
+                        <ColorModeIconDropDown size= "medium" /> /* Add Later */
+
+                        <IconButton aria-label="Menu Button" onClick={ToggleButton(true)}>
+                            <MenuIcon /> /* Add Later */
+                        </IconButton>
+
+                    </Box>
                 </StyledToolbar>
             </Container>
 
