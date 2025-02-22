862b2a825ea8d19cdeba09a9c69502e7e1b342ec
Yuri <142381932+Yurei04@users.noreply.github.com>
2025-02-22 21:37:00 +0800

    Extend Help Project
    - Initialization for web extension
    - change font concept

---
+++ opitualre-extend-help-project/content.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener((Request, sender, sendResponse) => {
+    if (request.action === "toggleDyslexicFont") {
+        if (request.state) {
+
+        } else {
+            
+        }
+    }
+});
